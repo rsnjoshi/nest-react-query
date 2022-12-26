5c9584db1e64a52d0bdd5b253a08058e380d6488
roshan.joshi <roshan.joshi@cotiviti.com>
2022-12-26 13:31:28 +0000

    new machine changes

--- server_t/src/main.ts
+++ server_t/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3500);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
